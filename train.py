c86664bd787e88ac10ccd9006b9d7a6204838e43
tabularasa066a <tabularasa066a@gmail.com>
2020-09-26 07:09:38 +0900

    rev parameters

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ import torch.optim as optim
 
 TEST_DATA_SIZE = 100000  # テストデータのサイズ
 MINIBATCH_SIZE = 1000  # ミニバッチサイズ
+MINIBATCH_SIZE = 256  # ミニバッチサイズ
 EVALUATION_SIZE = 1000  # 評価のときのデータサイズ
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -20,8 +21,8 @@ print(device)
 
 def train(train_x, train_y, model, optimizer,loss_func, epoch, ITERATION_NUM, MINIBATCH_SIZE):
     # model.train()
-    for i in range(ITERATION_NUM):
-    # for i in range(0, train_x.shape[0], MINIBATCH_SIZE):
+    #for i in range(ITERATION_NUM):
+    for i in range(0, train_x.shape[0], MINIBATCH_SIZE):
         ## 学習: 重みのアップデート
         # 学習データをランダムに抽出
         # np.random.choice(a, n, replace=False): 0以上a以下の値からn個の値をランダムに抽出(重複無し)
